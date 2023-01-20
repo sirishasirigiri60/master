871777109e187d86fa5d23877d3c05c71f8b001a
sirishasirigiri60 <122536099+sirishasirigiri60@users.noreply.github.com>
2023-01-20 08:05:06 +0530

    Updated to take run-time count and search

--- scrape_twitter.py
+++ scrape_twitter.py
@@ -3,8 +3,10 @@ import pandas as pd
 
 tweets_list = []
 
-for i,tweet in enumerate(sntwitter.TwitterSearchScraper('#Accenture since:2023-01-01').get_items()):
-    if i>9999:
+count = int(input('Enter the maximum count of tweets '))
+search = input('Enter the search ')
+for i,tweet in enumerate(sntwitter.TwitterSearchScraper(search).get_items()):
+    if i>count:
         break
     tweets_list.append([tweet.date, tweet.id, tweet.url, tweet.content, 
                          tweet.user.username, tweet.replyCount, 
@@ -14,3 +16,5 @@ tweets_df = pd.DataFrame(tweets_list, columns=['Tweet Date', 'Tweet Id', 'URL',
                           'Content', 'Username', 'Reply Counts', 'Retweet Count',
                           'Language', 'Source', 'Like Count'])
 display(tweets_df)
+print('Scraped the data')
+tweets_df.to_csv('twitter_data_'+search+'.csv',index=False)
